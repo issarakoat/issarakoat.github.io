5bea2df3155532ecc1537c76381018f61a131cab
issarakoat <weeravut.issarakoat@gmail.com>
2020-05-08 11:07:29 -0600

    Updates

---
+++ precache-manifest.bab5409de32dceaba96c058cb5412011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17970587daa1a59d36b7a30c09e0bba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b12bf7fde87b9db089ef",
+    "url": "/static/css/main.b9b00b24.chunk.css"
+  },
+  {
+    "revision": "961301e33cc380de7a46",
+    "url": "/static/js/2.b3a1111c.chunk.js"
+  },
+  {
+    "revision": "b12bf7fde87b9db089ef",
+    "url": "/static/js/main.51fca1ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "457ea2a1572bd7c13ec1d4306fdbc246",
+    "url": "/static/media/cs_resume.457ea2a1.pdf"
+  }
+]);
